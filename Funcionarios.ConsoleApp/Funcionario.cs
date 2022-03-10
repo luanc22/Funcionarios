35ef21850ef3838c08e168258ed0e710b2e29832
Luan Vale <luancabral5566@outlook.com>
2022-03-10 15:54:34 -0300

    Add project files.

---
+++ Funcionarios.ConsoleApp/Funcionario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios.ConsoleApp
+{
+    public class Funcionario
+    {
+        public int id;
+        public string nome;
+        public string departamento;
+        public double salario;
+        public DataDeEntrada dataDeEntrada;
+        public string rg;
+        public bool estaNaEmpresa;
+
+        public double AumentaSalario(double aumento)
+        {
+            salario = salario + aumento;
+            return salario;
+        }
+
+        public void Demite()
+        {
+            id = 0;
+            nome = "";
+            departamento = "";
+            salario = 0;
+            rg = "";
+            estaNaEmpresa = false;
+        }
+
+        public void Mostra()
+        {
+            Console.WriteLine();
+            Console.WriteLine("ID do funcionario: " + id);
+            Console.WriteLine("Nome do funcionario: " + nome);
+            Console.WriteLine("Departamento do funcionario: " + departamento);
+            Console.WriteLine("Salario do funcionario: " + salario);
+            Console.WriteLine("RG do Funcionario: " + rg);
+            Console.WriteLine("Dia de entrada: " + dataDeEntrada.dia);
+            Console.WriteLine("Mes de entrada: " + dataDeEntrada.mes);
+            Console.WriteLine("Ano de entrada: " + dataDeEntrada.ano);
+
+            if (estaNaEmpresa == true)
+            {
+                Console.WriteLine("Funcionario se encontra na empresa.");
+            }
+            else
+            {
+                Console.WriteLine("Funcionario nao se encontra na empresa.");
+            }
+        }
+    }
+
+    public class DataDeEntrada
+    {
+        public int dia;
+        public int mes;
+        public int ano;
+    }
+}
---
+++ Funcionarios.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Funcionarios.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime agora = new System.DateTime();
+            agora = DateTime.Now;
+            int horaAgora = agora.Hour;
+
+            Funcionario funcionario1 = new Funcionario();
+            DataDeEntrada data1 = new DataDeEntrada();
+
+            funcionario1.id = 1;
+            funcionario1.nome = "Luan";
+            funcionario1.dataDeEntrada = data1;
+            funcionario1.rg = "39.149.402-8";
+            funcionario1.departamento = "Dev Jr.";
+            funcionario1.salario = 2500;
+            data1.dia = 11;
+            data1.mes = 3;
+            data1.ano = 2022;
+
+            if(horaAgora >= 13 && horaAgora <= 19)
+            {
+                funcionario1.estaNaEmpresa = true;
+            }
+            else
+            {
+                funcionario1.estaNaEmpresa = false;
+            }
+
+            funcionario1.Mostra();
+            Console.ReadLine();
+        }
+    }
+}
